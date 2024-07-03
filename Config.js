a0ca6a868abcb68b9b7191e1572c008d144afa7b
Mountyare5 <mountyare538@gmail.com>
2024-07-04 02:13:36 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,18 +3,18 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
-global.ytname = "YT: AyushBotz.inc"
+global.ownernumber = [ process.env.OWNER_NUMBER |"252627478165"],
+global.ownername = process.env.OWNER_NAME || "BIGMANRASHKA",
+global.ytname = "YT: rashka"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "somalia"
 
 global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.stickername = process.env.STICKER || "💙𓆩‎bigman_✰",
 global.packname = 'Sticker By'
-global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.author = process.env.AUTHOR || "💙𓆩‎✘bigman ✰",
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
